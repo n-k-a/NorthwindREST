cde9a8eb8b44ed2fe32051f20d800dc5e1f6da7a
n-k-a <nkemakwari@gmail.com>
2021-10-21 16:33:25 +0100

    Created controllers for all non-join tables.

--- src/main/java/org/sparta/northwindrest/controllers/CategoryController.java
+++ src/main/java/org/sparta/northwindrest/controllers/CategoryController.java
@@ -3,16 +3,16 @@ package org.sparta.northwindrest.controllers;
 import org.sparta.northwindrest.entities.CategoryEntity;
 import org.sparta.northwindrest.entities.OrderEntity;
 import org.sparta.northwindrest.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@RestController
 public class CategoryController {
+    @Autowired
     private CategoryRepository categoryRepository;
     public CategoryController(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
